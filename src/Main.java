54da73381c71b8b9b70fcfa810df131be64ddb08
Vasya Golovastov <VSGolovastov@gmail.com>
2021-11-29 09:52:14 +0400

    AddTimeNow9312911

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Дата и время: " + LocalDateTime.now());
+        System.out.println();
     }
 }
